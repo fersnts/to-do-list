e8fbf2ac287af5767f61bf020c83ab3d6009131e
Fernanda Santos <fdsantos.dev@gmail.com>
2026-09-04 15:30:51 +0100

    Penúltima modificação
    falta adicionar trava para não adicionar tarefa vazia

--- inicial/scripts.js
+++ inicial/scripts.js
@@ -9,9 +9,13 @@ let minhaListaDeItens = []
 
 // 3. ADICIONAR O QUE FOI DIGITADO NO ARRAY
 function adicionarNovaTarefa() {
-    minhaListaDeItens.push(input.value)
-    mostrarTarefas()  // Atualiza o que aparece na tela
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluido: false
+    })
     input.value = ''
+    mostrarTarefas()  // Atualiza o que aparece na tela
+    
 }
 
 // 4. MOSTRAR TAREFAS NA TELA
@@ -20,15 +24,15 @@ function mostrarTarefas() {
     // Começa vazio e vai acumulando as <li>
     let novaLi = ''
 
-    // Percorre cada tarefa do array
-    minhaListaDeItens.forEach((tarefa) => {
+    // Percorre cada tarefa do array + poe o index
+    minhaListaDeItens.forEach((item, posicao) => {
         // Cria uma <li> para cada tarefa (o novaLi = novaLi é para ele sempre somar e não substituir o antigo)
         novaLi = novaLi + ` 
         
-            <li class="task">
-            <img src="./img/check.png" alt="check">
-            <p>${tarefa}</p>
-            <img src="./img/trash.png" alt="trash">
+            <li class="task ${item.concluida && "done"}">
+            <img src="./img/check.png" alt="check" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img src="./img/trash.png" alt="trash" onclick="deletarItem(${posicao})"> 
             </li>
             `
     })
@@ -36,6 +40,35 @@ function mostrarTarefas() {
     // Coloca todas as <li> dentro da lista no HTML
     listaCompleta.innerHTML = novaLi
 
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+
 }
 // 5. QUANDO CLICAR NO BOTÃO ADICIONA A TAREFA
 button.addEventListener('click', adicionarNovaTarefa)
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        adicionarNovaTarefa()
+    }
+})
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1)
+    mostrarTarefas()
+    
+    }
+    
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida 
+    mostrarTarefas()
+}
+
+function recarregarTarefas(){
+    const tarefasLocalStorage = localStorage.getItem('lista')
+
+    if(tarefasLocalStorage){
+    minhaListaDeItens = JSON.parse(tarefasLocalStorage)
+    }
+    mostrarTarefas()
+}
+
+recarregarTarefas()
